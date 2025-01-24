89165b7427a285c21755a41c5c8f0af2e8660458
ChefCarlos <carlind95@gmail.com>
2025-01-24 22:15:15 +0000

    queueify message;

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import discord
+import asyncio
 import os
 from classes.message_handler import MessageHandler
 from classes.config_manager import configManager
@@ -7,21 +8,30 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-
+message_queue = asyncio.Queue()
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
+    client.loop.create_task(process_messages())
 
 @client.event
 async def on_message(message):
-    handler = MessageHandler(message, client)
+    await message_queue.put(message)
+    
+async def process_messages():
+    while True:
+        message = await message_queue.get()
+        handler = MessageHandler(message, client)
 
-    if handler.should_process_message() == False:
-        return
+        if handler.should_process_message() == False:
+            continue
 
-    async with message.channel.typing():
-        await handler.handle_message()
+        async with message.channel.typing():
+            await handler.handle_message()
+        message_queue.task_done()
+    
+    
 
 token = configManager().get_setting("discord_token")
 client.run(token)
