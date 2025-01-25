c7092a9b78c6837da3f1efb9b85f4480decb0482
Dan Gowing <danielgowing95@gmail.com>
2025-01-25 20:48:20 +0200

    Revert "queueify message;"
    This reverts commit 89165b7427a285c21755a41c5c8f0af2e8660458.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 import discord
-import asyncio
 import os
 from classes.message_handler import MessageHandler
 from classes.config_manager import configManager
@@ -8,30 +7,21 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-message_queue = asyncio.Queue()
+
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
-    client.loop.create_task(process_messages())
 
 @client.event
 async def on_message(message):
-    await message_queue.put(message)
-    
-async def process_messages():
-    while True:
-        message = await message_queue.get()
-        handler = MessageHandler(message, client)
+    handler = MessageHandler(message, client)
 
-        if handler.should_process_message() == False:
-            continue
+    if handler.should_process_message() == False:
+        return
 
-        async with message.channel.typing():
-            await handler.handle_message()
-        message_queue.task_done()
-    
-    
+    async with message.channel.typing():
+        await handler.handle_message()
 
 token = configManager().get_setting("discord_token")
 client.run(token)
