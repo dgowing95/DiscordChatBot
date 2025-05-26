4179090b8c2588b4592b51c6b224ec55f4d801d8
Dan Gowing <danielgowing95@gmail.com>
2025-05-26 22:13:35 +0300

    feat(bot): improve image generation

--- app/classes/image_handler.py
+++ app/classes/image_handler.py
@@ -3,6 +3,10 @@ from diffusers import DiffusionPipeline
 
 class ImageHandler:
     pipe: DiffusionPipeline = None
+    refiner : DiffusionPipeline = None
+    # Define how many steps and what % of steps to be run on each experts (80/20) here
+    n_steps: int = 40
+    high_noise_frac: float = 0.8
 
     def __init__(self):
         pass
@@ -10,15 +14,41 @@ class ImageHandler:
     async def setup(self):
         if ImageHandler.pipe is None:
             print("Initializing ImageHandler...")
-            model = os.environ.get("IMAGE_MODEL", "stable-diffusion-v1-5/stable-diffusion-v1-5")
-            ImageHandler.pipe = DiffusionPipeline.from_pretrained(model, torch_dtype=torch.float16, cache_dir="/home/.cache/huggingface/hub", safety_checker=None)
-            ImageHandler.pipe.to("cuda")
-            print(f"ImageHandler initialized with model: {model}")
+
+            # load both base & refiner
+            ImageHandler.pipe = DiffusionPipeline.from_pretrained(
+                "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True, cache_dir="/home/.cache/huggingface/hub", safety_checker=None
+            )
+            ImageHandler.pipe.enable_model_cpu_offload()
+            ImageHandler.refiner = DiffusionPipeline.from_pretrained(
+                "stabilityai/stable-diffusion-xl-refiner-1.0",
+                text_encoder_2=ImageHandler.pipe.text_encoder_2,
+                cache_dir="/home/.cache/huggingface/hub",
+                vae=ImageHandler.pipe.vae,
+                torch_dtype=torch.float16,
+                use_safetensors=True,
+                variant="fp16",
+            )
+            ImageHandler.refiner.enable_model_cpu_offload()
+
+
+            print(f"ImageHandler initialized")
 
     async def generate_image(self, prompt):
         await self.setup()
         try:
-            image = ImageHandler.pipe(prompt).images[0]
+            image = ImageHandler.pipe(
+                prompt=prompt,
+                num_inference_steps=ImageHandler.n_steps,
+                denoising_end=ImageHandler.high_noise_frac,
+                output_type="latent",
+            ).images[0]
+            image = ImageHandler.refiner(
+                prompt=prompt,
+                num_inference_steps=ImageHandler.n_steps,
+                denoising_start=ImageHandler.high_noise_frac,
+                image=image,
+            ).images[0]
             return self.return_discord_file(image)
         except Exception as e:
             print(f"Error generating image: {e}")
