0ebc6243e253144a14505e938f74d1b83a88d318
Dan Gowing <danielgowing95@gmail.com>
2025-05-31 22:34:50 +0300

    feat(image): Add iamge modification command

--- diffusionservice/main.py
+++ diffusionservice/main.py
@@ -4,29 +4,27 @@ from pydantic import BaseModel
 from PIL import Image
 from text_to_image_handler import TextToImageHandler
 from image_to_image_handler import ImageToImageHandler
-import io
+import io, gc
 
 app = FastAPI()
-text_to_image_handler = TextToImageHandler()
-image_to_image_handler = ImageToImageHandler()
 
 class PromptRequest(BaseModel):
     prompt: str
 
 @app.post("/text-image")
 async def textToImage(prompt: PromptRequest):
+    text_to_image_handler = TextToImageHandler()
     image_bytes = await text_to_image_handler.generate_image(prompt.prompt)
     if image_bytes is None:
         raise HTTPException(status_code=500, detail="Image generation failed")
     
+    text_to_image_handler.release_resources()
+    gc.collect()
     return StreamingResponse(io.BytesIO(image_bytes), media_type="image/png")
 
 @app.post("/image-image")
-async def imageToImage(
-    prompt: str = Form(...),
-    strength: float = Form(0.75),
-    file: UploadFile = File(...)
-):
+async def imageToImage(prompt: str = Form(...), strength: float = Form(0.75), file: UploadFile = File(...)):
+    image_to_image_handler = ImageToImageHandler()
     init_bytes = await file.read()
     try:
         init_image = Image.open(io.BytesIO(init_bytes)).convert("RGB")
@@ -36,5 +34,6 @@ async def imageToImage(
     output_bytes = await image_to_image_handler.image_to_image(prompt, init_image)
     if output_bytes is None:
         return {"error": "Image generation failed"}
-
+    image_to_image_handler.release_resources()
+    gc.collect()
     return StreamingResponse(output_bytes, media_type="image/png")
--- diffusionservice/text_to_image_handler.py
+++ diffusionservice/text_to_image_handler.py
@@ -2,43 +2,36 @@ import os, torch, io, gc
 from diffusers import DiffusionPipeline
 
 class TextToImageHandler:
-    pipe: DiffusionPipeline = None
-
-    def __init__(self):
+    def _init__(self):
         pass
 
     async def setup(self):
-        if TextToImageHandler.pipe is None:
-            print("Initializing ImageHandler...")
-            TextToImageHandler.pipe = DiffusionPipeline.from_pretrained(
-                os.environ.get("IMAGE_MODEL", "stabilityai/stable-diffusion-xl-base-1.0"),
-                torch_dtype=torch.float16,
-                variant="fp16",
-                use_safetensors=True,
-                cache_dir="/home/.cache/huggingface/hub",
-                device_map="balanced"
-            )
-            TextToImageHandler.pipe.safety_checker = None
-            print(f"ImageHandler initialized")
+        print("Initializing ImageHandler...")
+        self.pipe = DiffusionPipeline.from_pretrained(
+            os.environ.get("IMAGE_MODEL", "stabilityai/stable-diffusion-xl-base-1.0"),
+            torch_dtype=torch.float16,
+            variant="fp16",
+            use_safetensors=True,
+            cache_dir="/home/.cache/huggingface/hub",
+            device_map="balanced"
+        )
+        self.pipe.safety_checker = None
+        print(f"ImageHandler initialized")
 
     async def generate_image(self, prompt: str) -> bytes:
         await self.setup()
         try:
-            image = TextToImageHandler.pipe(prompt=prompt).images[0]
-            #self.release_resources()
+            image = self.pipe(prompt=prompt).images[0]
             return self._image_to_bytes(image)
         except Exception as e:
             print(f"Error generating image: {e}")
-            #self.release_resources()
             return None
 
     def release_resources(self):
-        if TextToImageHandler.pipe is not None:
-            print("Releasing resources...")
-            del TextToImageHandler.pipe
-            TextToImageHandler.pipe = None
-            torch.cuda.empty_cache()
-            gc.collect()
+        print("Releasing resources...")
+        del self.pipe
+        self.pipe = None
+        torch.cuda.empty_cache()
 
     def _image_to_bytes(self, image):
         byte_array = io.BytesIO()
